80222e292e38ac8c437907b2a8ebb495e0c868a7
Sithanga Uwage <suwage@geometricresults.co.uk>
2020-09-08 16:05:51 +0100

    work pending

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -15,3 +15,70 @@ describe('Homepage', function () {
     }
   });
 });
+
+// Stage 2
+
+// const assert = require('assert');
+// describe('Homepage', function () { 
+//   it('should load properly', function () {
+//         // load the page
+//     browser.url('./');
+//     // Get the title of the homepage, should be 'Conduit'
+//     assert.strictEqual(browser.getTitle(), 'Conduit'); // Click the 'Sign in' navigation link
+//     $('=Sign in').click();
+//     // Get the URL of the sign in page. It should include 'login'
+//     assert.strictEqual(browser.getUrl(), 'http://localhost:8080/login'); 
+//   });
+// });
+
+
+// Stage 3
+
+// describe('Homepage', function () { 
+//   it('should load properly', function () {
+//       // load the page
+//     browser.url('./');
+//     // Get the title of the homepage, should be 'Conduit'
+//     expect(browser).toHaveTitle('Conduit'); // Click the 'Sign in' navigation link
+//     $('=Sign in').click();
+//     // Get the URL of the sign in page. It should include 'login'
+//     expect(browser).toHaveUrl('http://localhost:8080/login'); 
+//   });
+// });
+
+
+// Stage 4
+
+// describe('Homepage', function () {
+//   it('should load properly', function () {
+//     // load the page
+//     browser.url('./');
+//     // Get the title of the homepage, should be 'Conduit'
+//     expect(browser).toHaveTitle('Conduit'); // Click the 'Sign in' navigation link
+//     $('=Sign in').click();
+//     // Get the URL of the sign in page. It should include 'login'
+//     expect(browser).toHaveUrl('/login', { containing: true }); 
+//   });
+// });
+
+// Stage 5
+
+// describe('Homepage', function () {
+//   it('should load properly', function () {
+//     // load the page
+//     browser.url('./');
+//     // Get the title of the homepage, should be 'Conduit'
+//     expect(browser).toHaveTitle('Conduit'); 
+//     // Click the 'Sign in' navigation link
+//     $('=Sign in').click();
+//     // Get the URL of the sign in page. It should include 'login'
+//     expect(browser).toHaveUrl('/login', { containing: true }); 
+
+//     $((//*[contains(text(),"conduit")])[1]).click()
+//     expect(browser).toHaveUrl('');
+
+//     footer $('//span[@class="attribution"]')
+//     popular tags $$('//div[@class="tag-list"]//a')
+//     popular tags with text $('//p[text()="Popular Tags"]/..//a')
+//   });
+// });
