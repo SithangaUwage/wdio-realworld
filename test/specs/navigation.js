c630dac9d399e553a02ef61c82542305f8d718ad
Sithanga Uwage <suwage@geometricresults.co.uk>
2020-10-08 16:22:17 +0100

    section 2.3

--- test/specs/navigation.js
+++ test/specs/navigation.js
@@ -15,13 +15,13 @@ describe('Homepage', function () {
       throw new Error('URL of "login" page should be correct');
     }
   });
-}); 
+});
 */
 
 // Stage 2
 /*
 const assert = require('assert');
-describe('Homepage', function () { 
+describe('Homepage', function () {
   it('should load properly', function () {
         // load the page
     browser.url('./');
@@ -29,15 +29,14 @@ describe('Homepage', function () {
     assert.strictEqual(browser.getTitle(), 'Conduit'); // Click the 'Sign in' navigation link
     $('=Sign in').click();
     // Get the URL of the sign in page. It should include 'login'
-    assert.strictEqual(browser.getUrl(), 'http://localhost:8080/login'); 
+    assert.strictEqual(browser.getUrl(), 'http://localhost:8080/login');
   });
 });
 */
 
-
 // Stage 3
 /*
-describe('Homepage', function () { 
+describe('Homepage', function () {
   it('should load properly', function () {
       // load the page
     browser.url('./');
@@ -45,11 +44,10 @@ describe('Homepage', function () {
     expect(browser).toHaveTitle('Conduit'); // Click the 'Sign in' navigation link
     $('=Sign in').click();
     // Get the URL of the sign in page. It should include 'login'
-    expect(browser).toHaveUrl('http://localhost:8080/login'); 
+    expect(browser).toHaveUrl('http://localhost:8080/login');
   });
 });
 */
-
 
 // Stage 4
 /*
@@ -61,31 +59,31 @@ describe('Homepage', function () {
     expect(browser).toHaveTitle('Conduit'); // Click the 'Sign in' navigation link
     $('=Sign in').click();
     // Get the URL of the sign in page. It should include 'login'
-    expect(browser).toHaveUrl('/login', { containing: true }); 
+    expect(browser).toHaveUrl('/login', { containing: true });
   });
 });
 */
 
 // Stage 5
-
+/*
 describe('Homepage', function () {
   it('should load properly', function () {
     // load the page
     browser.url('./');
     // Get the title of the homepage, should be 'Conduit'
-    expect(browser).toHaveTitle('Conduit'); 
+    expect(browser).toHaveTitle('Conduit');
     // Click the 'Sign in' navigation link
     $('=Sign in').click();
     // Get the URL of the sign in page. It should include 'login'
-    expect(browser).toHaveUrl('/login', { containing: true }); 
+    expect(browser).toHaveUrl('/login', { containing: true });
 
     $('(//a[contains(text(),"conduit")])[1]').click()
     expect(browser).toHaveUrl('http://localhost:8080/');
   });
 });
+*/
 
 // Stage 5 Challenge
 // footer $('//span[@class="attribution"]')
 // popular tags $$('//div[@class="tag-list"]//a')
 // popular tags with text $('//p[text()="Popular Tags"]/..//a')
-
