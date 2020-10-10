447cdea6ca5e4be183c132a82f9f9c24d6440bb1
Sithanga <sithanga.uwage@gmail.com>
2020-10-10 13:09:05 +0100

    Section 2.4 Complete

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,18 +1,32 @@
+// Mocha
+/*
+  - it.skip() => skips the test
+  - it.only() => run single test
+  - describe.only() => run single describe block
+*/
+const Auth = require('../pageObjects/Auth.page');
+const auth = new Auth();
+  
 describe('Login Page', function () {
-  it('should let you log in', function () {
-    browser.url('./login')
-
-    $('input[type="email"]').setValue('demo@learnwebdriverio.com')
-    $('input[type="password"]').setValue('wdiodemo')
+  beforeEach(function () {
+    browser.url('./login');
+  });
     
-    const $signIn = $('button*=Sign in');
-    $signIn.click();
-    $signIn.waitForDisplayed({ reverse: true });
-
-    $('a[href="/my-feed"]').waitForDisplayed()
-
-    expect($('.error-messages li')).not.toBeExisting();
+  it('should let you log in', function () {
+    auth.login('demo@learnwebdriverio.com', 'wdiodemo');
+    expect(auth.$errorMessages).not.toBeExisting();
   })
-// should error with a missing username
-// should error with a missing password
+
+  it('should error with a missing username', function () {
+    auth.login('', 'wdiodemo');
+    // assert that error message is showing
+    expect(auth.$errorMessages).toHaveText(`email can't be blank`);
+
+  });
+  
+  it('should error with a missing password', function () {
+    auth.login('demo@learnwebdriverio.com', '');
+    // assert that error message is showing
+    expect(auth.$errorMessages).toHaveText(`password can't be blank`);
+  });
 })
