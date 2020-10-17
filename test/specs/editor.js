78eae707d470908d23d6e30a3fe7fe36dc93e1b0
Sithanga <sithanga.uwage@gmail.com>
2020-10-17 12:15:00 +0100

    Creating article and checking the url for the article title

--- test/specs/editor.js
+++ test/specs/editor.js
@@ -1,9 +1,14 @@
+const Chance = require('chance');
 const Auth = require('../pageObjects/Auth.page');
 const Editor = require('../pageObjects/Editor.page');
+const Article = require('../pageObjects/Article.page');
+
 const { user1 } = require('../fixtures/users');
 
 const auth = new Auth();
 const editor = new Editor();
+const chance = new Chance();
+const article = new Article();
 
 describe('Post Editor', function () {
 	before(function () {
@@ -12,7 +17,8 @@ describe('Post Editor', function () {
 	});
 	beforeEach(function () {
 		editor.load();
-	});
+  });
+
 	it('should load page properly', function () {
 		expect(browser).toHaveUrl(editor.url.href);
 		expect(editor.$title).toBeExisting();
@@ -20,5 +26,24 @@ describe('Post Editor', function () {
 		expect(editor.$body).toBeExisting();
 		expect(editor.$tags).toBeExisting();
 		expect(editor.$publish).toBeExisting();
-	});
+  });
+  
+  it('should let you publish a new post', function () {
+    const articleDetails = {
+      title: chance.sentence({ words: 3 }),
+      description: chance.sentence({ words: 7 }),
+      body: chance.paragraph({ sentences: 4 }),
+      tags: [chance.word(), chance.word()]
+    };
+
+    editor.submitArticle(articleDetails);
+
+    const slug = articleDetails.title
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^\w-]+/g, '');
+
+    expect(browser).toHaveUrl(`articles/${slug}`, { containing: true });
+    $('button*=Delete Article').click();
+  })
 });
