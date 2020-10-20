05c31d111380bb0823a0c15d267f02de8604e7f3
Sithanga <sithanga.uwage@gmail.com>
2020-10-20 19:19:22 +0100

    section 2.9 complete

---
+++ test/pageObjects/Tag.page.js
@@ -0,0 +1,7 @@
+const Home = require('./Home.page');
+class Tag extends Home {
+  constructor(tagName) {
+    super('./tag/' + tagName);
+  }
+}
+module.exports = Tag;
--- utils/Api.js
+++ utils/Api.js
@@ -6,12 +6,33 @@ class Api {
       responseType: 'json'
     });
   }
+
   getAuthToken({ email, password }) {
     return this.client
     .post('users/login', {
       json: { user: { email, password } }
     })
     .then((response) => response.body.user.token);
+  }
+
+  async createArticle(user, details) {
+    const token = await this.getAuthToken(user);
+    const response = await this.client.post('articles', {
+    json: {
+      article: details
+      },
+      headers: {
+        Authorization: `Token ${token}`
+        }
+      });
+      return response.body.article;
+    }
+
+  async deleteArticle(user, slug) {
+    const token = await this.getAuthToken(user);
+    return this.client.delete(`articles/${slug}`, {
+      headers: { Authorization: `Token ${token}` }
+    });
   }
 }
 module.exports = Api;
